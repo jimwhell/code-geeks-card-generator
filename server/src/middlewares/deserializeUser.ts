bc715df53e47cfceb332a320f4e12dd448fbf60b
jimwell <jimwelllmanalo@gmail.com>
2025-06-01 00:42:36 +0800

    Remove log statements

--- server/src/middlewares/deserializeUser.ts
+++ server/src/middlewares/deserializeUser.ts
@@ -1,20 +1,19 @@
 import { NextFunction, Request, Response } from "express";
 import { verifyJWT } from "../utils/jwt.utils";
 import {
-  findSessions,
   findValidSession,
   reIssueAccessToken,
 } from "../services/sessions.service";
-import { doesNotMatch } from "assert";
+import log from "../utils/logger";
 
 const deserializeUser = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  const authorizationHeader = req.headers.authorization;
-  console.log("authorization header: ", authorizationHeader);
+  const authorizationHeader: string | undefined = req.headers.authorization;
   if (!authorizationHeader) {
+    log.info("No authorization header found");
     res.status(401).send("Unauthorized access");
     return;
   }
@@ -23,31 +22,34 @@ const deserializeUser = async (
   const accessToken = authorizationHeader.replace(/^Bearer\s/, "");
   const refreshToken = req.headers["x-refresh"];
 
+  //verify access token
   const jwtResult = verifyJWT(accessToken);
 
   if (!jwtResult) {
+    log.info("Invalid access token");
     res.status(401).send("Invalid access token");
     return;
   }
 
+  //extract decoded data from access token
   const { decoded, expired } = jwtResult;
 
-  console.log("expired: ", expired);
-  console.log("decoded: ", decoded);
-
-  if (!decoded && !refreshToken) {
+  if (decoded === null && !refreshToken) {
     res.status(401).send("Unauthorized access.");
     return;
   }
+
+  console.log("Decoded: ", decoded);
 
   //if access token is expired, verify refreshToken then create a new one.
   if (expired && refreshToken && typeof refreshToken === "string") {
     const newAccessToken = await reIssueAccessToken({ refreshToken });
 
-    if (!newAccessToken) {
+    if (newAccessToken === null) {
       res.status(401).send("Invalid session");
       return;
     }
+
     //set new access token to header
     res.setHeader("x-access-token", newAccessToken);
 
@@ -70,14 +72,17 @@ const deserializeUser = async (
     return;
   }
 
-  const sessionId = decoded.session;
+  //extract session id from access token
+  const sessionId: string = decoded.session;
 
+  //find a valid session that matches the ID of the current session
   const session = await findValidSession(sessionId);
 
+  //if no valid session was found, even if the token was valid, restrict access
   if (!session) {
     res.status(401).send("Session revoked. Authentication required.");
+    return;
   }
-
 
   //if token is valid:
   //include admin details to res.locals
