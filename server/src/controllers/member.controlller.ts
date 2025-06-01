8c402a308eb4046cb1019848d9eaa732a282e4e6
jimwell <jimwelllmanalo@gmail.com>
2025-06-01 23:26:31 +0800

    Replace 500 responses with thrown errors

--- server/src/controllers/member.controlller.ts
+++ server/src/controllers/member.controlller.ts
@@ -1,27 +1,55 @@
 import { Request, Response, NextFunction } from "express";
-import { getAllMembers } from "../services/member.service";
-import { Member } from "../models/member.model";
+import { getAllMembers } from "../models/member.model";
+import {
+  Member,
+  MembersQuery,
+  MembersQueryResult,
+  PaginatedMemberResponse,
+} from "../types/member.types";
 import log from "../utils/logger";
+import { off } from "process";
+import { totalmem } from "os";
 
 export async function getAllMembersHandler(
-  req: Request,
-  res: Response,
+  req: Request<{}, {}, {}, MembersQuery>,
+  res: Response<PaginatedMemberResponse>,
   next: NextFunction
 ) {
   try {
-    const members: Member[] | null = await getAllMembers();
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 20;
 
-    if (!members) {
-      res.status(500).json({ message: "Failed to retrieve members" });
-      return;
+    const offset: number = (page - 1) * limit;
+
+    const result: MembersQueryResult | null = await getAllMembers(
+      limit,
+      offset
+    );
+
+    if (!result) {
+      throw new Error("Failed to retrieve members");
     }
 
-    if (members === null) {
-      res.status(404).json({ message: "No members found" });
-      return;
+    if (result === null) {
+      throw new Error("No members found");
     }
 
-    res.status(200).json({ members: members });
+    let paginatedResponse: PaginatedMemberResponse = {
+      totalMembers: result.totalMembers,
+      totalPages: result.totalPages,
+      currentPage: page,
+      members: result.members,
+    };
+
+    if (page * limit < paginatedResponse.totalMembers) {
+      paginatedResponse.nextPage = page + 1;
+    }
+
+    if (offset > 0) {
+      paginatedResponse.previousPage = page - 1;
+    }
+
+    res.status(200).json(paginatedResponse);
   } catch (error) {
     log.error("Error in retrieving members: ", error);
     next(error);
